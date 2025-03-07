6753debdfd2946b7f32f19e300e0ef1a57a11899
Richard Lambert <hello@richardlambert.dev>
2025-03-07 11:04:48 +0000

    Add ex1.py

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# ex1.py
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study Drills
+print("Hello, Lilly!")
+
+# In Python, the `#` character is used to create comments.
+# The Python interpreter ingores everything from the `#` character to the end of the line.
+# Comments can be used to document code and to temporarily prevent a piece of code from running.
