4db55929b525fe6afb40f83658d3417225e7fbfe
Richard Lambert <hello@richardlambert.dev>
2025-03-11 11:54:04 +0000

    Add ex34.py

---
+++ 31-40/ex34.py
@@ -0,0 +1,12 @@
+# ex34.py - Accessing Elements of Lists
+
+animals = ["bear", "python3", "peackock", "kangaroo", "whale", "platypus"]
+
+print(f"The animal at index 1 is: {animals[1]}")  # python3
+print(f"The third animal (index 3 - 1) is: {animals[2]}")  # peacock
+print(f"The first animal (index 1 - 1) is: {animals[0]}")  # bear
+print(f"The animal at index 3 is: {animals[3]}")  # kangaroo
+print(f"The fifth animal (index 5 - 1) is: {animals[5 - 1]}")  # whale
+print(f"The animal at index 2 is: {animals[2]}")  # peacock
+print(f"The sixth animal (index 6 - 1) is: {animals[5]}")  # platypus
+print(f"The animal at index 4 is: {animals[4]}")  # whale
